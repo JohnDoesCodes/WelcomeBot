8247a49021559a7dbdb976484cd14ad2fd246e16
aeris <patrickyamut@yahoo.com>
2017-08-02 01:51:06 +0800

    Fixed error

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,3 @@
-exports.run = (message, client) {
+exports.run = (message, client) => {
   message.reply("Hello there, here are all my commands: https://github.com/JohnDoesCodes/WelcomeBot/blob/master/Commands.md")
 }
