241287befe708f2719d0504a41361342c1140a06
aeris <patrickyamut@yahoo.com>
2017-08-02 16:37:41 +0800

    Updated

--- commands/say.js
+++ commands/say.js
@@ -2,7 +2,8 @@ let Discord = require("discord.js")
 exports.run = (client, message, args) => {
   let embed = new Discord.RichEmbed();
   let suffix = message.content.split(' ').slice(1).join(' ');
-  embed.setDescription(`Requested by ${message.author.user}: ${suffix}`)
+  embed.setAuthor(`${message.author.avatarURL}`)
+  .setDescription(`**Requested by ${message.author.user.username}:** ${suffix}`)
   .setTimestamp()
   .setColor("#e3148a")
   message.channel.send({embed})
