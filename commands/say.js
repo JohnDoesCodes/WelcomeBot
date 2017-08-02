682650f77f32376ee64891c411bd339cac019bc6
aeris <patrickyamut@yahoo.com>
2017-08-02 16:39:49 +0800

    Updated - Fixing few errors

--- commands/say.js
+++ commands/say.js
@@ -2,9 +2,9 @@ let Discord = require("discord.js")
 exports.run = (client, message, args) => {
   let embed = new Discord.RichEmbed();
   let suffix = message.content.split(' ').slice(1).join(' ');
-  embed.setAuthor(`${message.author.avatarURL}`)
-  .setDescription(`**Requested by ${message.author.user.username}:** ${suffix}`)
+  embed.setAuthor("Say it please!")
+  .setDescription(`**Requested by ${message.author.tag}:** ${suffix}`)
   .setTimestamp()
   .setColor("#e3148a")
-  message.channel.send({embed})
+  message.channel.send({embed}).catch((err) => {message.channel.send(`:warning: **An error occurred.**\n${err}`); console.log(err)});
 }
