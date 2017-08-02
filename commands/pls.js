39f5de17692c5ab45cc2c6cc8777fbc8c34abc0d
aeris <patrickyamut@yahoo.com>
2017-08-02 16:29:47 +0800

    Fixing say command

--- commands/pls.js
+++ commands/pls.js
@@ -1,9 +1,10 @@
 let Discord = require("discord.js")
-exports.run = (client, message, args) => {
+exports.run = (client, message) => {
+  let args = message.content.split(' ').slice(1).join(' ');
   if (!args) return message.channel.send("Please specify an argument.\n`say`.")
   if (args === "say") {
     let embed = new Discord.RichEmbed();
-    let suffix = message.content.split(' ').slice(1).join(' ');
+    let suffix = message.content.split(' ').slice(2).join(' ');
     embed.setDescription(`Requested by ${message.author.user}: ${suffix}`)
     message.channel.send({embed})
   }
